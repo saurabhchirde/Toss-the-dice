7456f7b16b1bcf65e2e68e926a416bc73254b7a8
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-26 16:09:27 +0530

    script: elements selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const dice1 = document.querySelector(".dice1");
+const dice2 = document.querySelector(".dice2");
+const btnToss = document.querySelector(".toss");
+const output = document.querySelector(".output");
